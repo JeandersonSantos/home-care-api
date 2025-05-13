a445bdf51451489b9d989ba4847a3a926e96171c
JeandersonSantos <jeandersonsantospaz@gmail.com>
2025-05-13 02:16:22 -0300

    Ajustando

---
+++ api/index.ts
@@ -0,0 +1,9 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import app from '../src/server';
+
+export default async function handler(
+  req: VercelRequest,
+  res: VercelResponse
+) {
+  return app(req, res);
+}
